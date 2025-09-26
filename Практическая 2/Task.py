d7aaf70d48d15fb80d4d0e040c219d1ac737f4d4
aaa-564 <telishevskiy06@mail.ru>
2025-09-26 12:03:40 +0300

    Update Task.py

--- Практическая 2/Task.py
+++ Практическая 2/Task.py
@@ -1,1 +1,10 @@
 
+Бинарная куча
+public class BinaryHeap<T extends Comparable<T>> {
+    private T[] heap;  // массив элементов
+    private int size;  // текущий размер
+
+Хэш-таблица
+public class HashMap<K,V> {
+    private Node<K,V>[] table;  // массив бакетов
+    private int capacity;  // емкость таблицы
