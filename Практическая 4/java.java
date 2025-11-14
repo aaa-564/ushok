dcaec554331a9fff67b2ce3d4791f649d951e9c1
aaa-564 <telishevskiy06@mail.ru>
2025-11-14 09:28:35 +0300

    Create java.java

---
+++ Практическая 4/java.java
@@ -0,0 +1,8 @@
+public class SumOfDigits {
+    public static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return Math.abs(n % 10) + sumOfDigits(n / 10);
+    }
+}
